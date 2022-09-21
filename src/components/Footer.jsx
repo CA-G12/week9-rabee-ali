6dfa4dfd619df09299a351bec26ab23c1ffe3a2c
alishanti98 <alialshanti90@gmail.com>
2022-09-22 00:04:23 +0300

    create header and footer section and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
-export class Footer extends React.Component{
-
-    render() {
-        return (<div>
+export class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="logo-in-footer">
             <a href="/">
-                <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="The Movie Database (TMDB)" width="154" height="20"/>
+              <img
+                src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+                alt="The Movie Database (TMDB)"
+                width="154"
+                height="20"
+              />
             </a>
-            <div>
-                <h2>Created by:</h2>
-                <h3>Rabee A. Al-Bhaisi</h3>
-                <h3>Ali Al-Shanti</h3>
-            </div>
-        </div>)
-    }
+          </div>
+          <div className="content-footer">
+            <h2>Created by:</h2>
+            <h3>
+              Rabee A. Al-Bhaisi <span> , </span> Ali Al-Shanti
+            </h3>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
